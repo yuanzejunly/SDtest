5b1c1bde62cb777421b66ebf4ca76b8b15983d4a
Zejun Yuan <yuanzejunly@gmail.com>
2017-09-27 22:23:16 -0700

    initial commit

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Health : MonoBehaviour {
+
+    public const int maxHealth = 7;
+
+    public int currentHealth = maxHealth;
+
+    public void TakeDamage()
+    {
+        currentHealth -= 1;
+        if (currentHealth <= 0) {
+            currentHealth = 0;
+            Debug.Log("Lose!");
+        }
+    }
+	
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerController : NetworkBehaviour
+{
+
+    public GameObject bulletPrefab;
+
+    public Transform bulletSpawn;
+
+
+
+
+    public override void OnStartLocalPlayer()
+    {
+        GetComponent<MeshRenderer>().material.color = Color.blue;
+    }
+
+
+    void Update()
+    {
+        if (!isLocalPlayer) return;
+
+        if (Input.GetKeyDown(KeyCode.Space)) CmdFire();
+
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+    }
+
+    [Command]
+    void CmdFire()
+    {
+        //spawn the bullet
+        var bullet = (GameObject)Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
+
+        //give bullet velocity
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 6;
+
+        //spawn the bullet on client side
+        NetworkServer.Spawn(bullet);
+
+        //destroy the bullet after 2 seconds
+        Destroy(bullet, 2.0f);
+    }
+}
